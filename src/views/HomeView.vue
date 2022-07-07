4434deacbf3a7c75a4c6ecdc5170217f07c0510a
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-07 16:10:13 -0300

    feat: burger form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <Banner/>
   <div class="main-container">
     <h1>Monte o seu burger</h1>
-    <p>form</p>
+    <BurguerForm/>
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue"
+import BurguerForm from "../components/BurgerForm.vue"
 
 export default{
   name: "Home",
   components:{
-    Banner
+    Banner,
+    BurguerForm
   }
 }
 </script>
