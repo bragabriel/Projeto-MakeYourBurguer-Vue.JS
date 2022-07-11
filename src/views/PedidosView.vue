d31541ad6a35dc66b7fc5e0fca3de9a9beea5962
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-11 15:40:55 -0300

    feat: dashboard

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
 
-<style>
+<script>
+import Dashboard from "../components/Dashboard.vue"
 
-</style>
+  export default{
+    name: "Pedidos",
+    components:{
+      Dashboard
+    }
+  }
+</script>
